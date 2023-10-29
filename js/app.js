fa2ee4c1167a422aaf072da789d77918e3a7fa79
ScottieCornett <scottie.cornett@gmail.com>
2023-10-29 12:09:29 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 // NOTHING HERE YET.
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelector('#open-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+btnOpenModal.addEventListener('click', openModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
